7016004d8e3ea50da824934d96b4aba3742a3e43
Gearoid Miggan <gmiggan2@hotmail.com>
2015-02-16 14:35:51 +0000

    Update Player.java List Types

--- edu/src/Player.java
+++ edu/src/Player.java
@@ -6,7 +6,7 @@ import java.util.Hashtable;
 
 public class Player {
 	// the key of colorGroups is the name of the color group.
-	private Hashtable<String, Integer> colorGroups = new Hashtable<String, Integer>();
+	private Hashtable colorGroups = new Hashtable();
 	private boolean inJail;
 	private int money;
 	private String name;
